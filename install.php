fac4204d3d56e5f279250ee5041cbafe8cfdefd2
Andrew56843 <58052792+Andrew56843@users.noreply.github.com>
2023-11-15 21:13:43 +0300

    Summary

--- install.php
+++ install.php
@@ -1,6 +1,13 @@
 <?php
+echo('1');
 require_once (__DIR__.'/crest.php');
 
+$stmt = $db->prepare("SELECT * FROM users WHERE `id` = ?");
+$id = 1;
+$stmt->execute([$id]);
+$userData = $stmt->fetch(PDO::FETCH_LAZY);
+echo($userData['unix_install']);
+exit;
 $result = CRest::installApp();
 if($result['rest_only'] === false):?>
     <head>
