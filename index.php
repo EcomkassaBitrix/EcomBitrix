d42d43e5953ed3fcad633d6a002b37ac472a7967
Andrew56843 <58052792+Andrew56843@users.noreply.github.com>
2023-11-21 01:14:01 +0300

    Summary

--- index.php
+++ index.php
@@ -123,6 +123,8 @@
                     $paySystemBitrix = bxGetAllPaySystem( $_REQUEST['member_id'] );
                     foreach ( $paySystemEcom as $value ) {
                         $namePaySys = str_replace('"', '', $value->description);
+                        echo($namePaySys);
+                        exit;
                         bxSalePaySystemAdd( $_REQUEST['member_id'], $codeHandler, $idPersonType, "Ecom: ".$namePaySys, $value->id, $paySystemBitrix );
                     }
                     //--------------------------------Выключение платёжки при отключении в ecom-------------------------------------
