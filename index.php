1aeb43d3dbcf86486a3f2de2fa6fea3eb87be699
Andrew56843 <58052792+Andrew56843@users.noreply.github.com>
2023-12-16 00:04:18 +0300

    Summary

--- index.php
+++ index.php
@@ -136,7 +136,7 @@
                     foreach ( $paySystemEcom as $value ) {
                         $namePaySys = str_replace('"', '', $value->description);
                         $resultAddPaySystem = bxSalePaySystemAdd( $_REQUEST['member_id'], $codeHandler, $idPersonType, "Ecom: ".$namePaySys, $value->id, $paySystemBitrix );
-                        if( is_object( $resultAddPaySystem ) ){
+                        if( is_array( $resultAddPaySystem ) ){
                             $arraybatch[$arraPor] =  ['method' => 'sale.paysystem.add', 'params'=> $resultAddPaySystem] ;
                             $arraPor++;
                         }
@@ -235,7 +235,7 @@
         <form action='index.php' method="post">
             <table style="font-size: 12px;width:415px;text-align: right;border: 2px solid #b7b7b7;border-radius: 15px; padding: 5px;">
                 <tr>
-                    <td style="color: #bfbfbf">Основные настройки.</td><td></td>
+                    <td style="color: #bfbfbf">Основные настройки0</td><td></td>
                 </tr>
                 <tr>
                     <td>Логин EcomKassa</td><td><input type="email" name="ecomLogin" style="width: 200px;text-align: center;" value="<? echo(htmlspecialchars($login, ENT_QUOTES, 'UTF-8')); ?>"></td>
