cff9b73861eb88934fc6fdbf9e2ef953ea3528a4
Andrew56843 <58052792+Andrew56843@users.noreply.github.com>
2023-12-15 22:48:07 +0300

    Summary

--- crest.php
+++ crest.php
@@ -291,26 +291,6 @@
 						}
 					}
 				}
-
-
-
-
-                try {
-                    $db = new PDO('mysql:host='.C_REST_MYSQL_HOST.';dbname='.C_REST_MYSQL_DBNAME, C_REST_MYSQL_USERNAME, C_REST_MYSQL_PASSWORD);
-                } catch (PDOException $e) {
-                    die();
-                }
-                $query = "INSERT INTO `bxLogs` (`logtxt`, `logtype`, `unix`) VALUES (:logtxt,:logtype,:unix)";
-                $params = [
-                    ':logtxt' => static::wrapData($arDataRest),
-                    ':logtype' => 'test',
-                    ':unix' => time()
-                ];
-                $stmt = $db->prepare($query);
-                $stmt->execute($params);
-
-
-
 				if(!empty($arDataRest))
 				{
 					$arDataRest[ 'halt' ] = $halt;
