ba7626f66629401bf861a39c8e92bc4a426b2f66
Ecomkassa <150684305+EcomkassaBitrix@users.noreply.github.com>
2025-11-01 20:54:59 +0300

    Update settings.php

--- settings.php
+++ settings.php
@@ -6,8 +6,8 @@ define('C_REST_CLIENT_SECRET','JnowMh03hp1ythtWRoMH5uD82OP5vWGVerx3PWjpxGmWw0FfN
 
 define('C_REST_MYSQL_DBNAME','ecomkassa');//
 define('C_REST_MYSQL_USERNAME','root');//
-define('C_REST_MYSQL_PASSWORD','YEXprx62868');//
-define('C_REST_MYSQL_HOST','node165261-ecomkassa-bitrix.mircloud.ru');//
+define('C_REST_MYSQL_PASSWORD','XohznQi6k9');//
+define('C_REST_MYSQL_HOST','node213843-ecomkassa-bitrix-test-2.mircloud.ru');//
 
 define('C_REST_FIELD_KASSANAME','URLFORPAYECOMKASSA');//
 define('C_REST_FIELD_PAYNAME','Ссылка оплаты EcomKassa');//
